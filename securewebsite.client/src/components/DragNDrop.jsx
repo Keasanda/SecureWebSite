321fa2955fec4db379f8fbc9a2d2fc4491ad9b5d
Keasanda <Mlungisi716@gmail.com>
2024-07-20 15:14:41 +0200

    Weekend Progress

--- securewebsite.client/src/components/DragNDrop.jsx
+++ securewebsite.client/src/components/DragNDrop.jsx
@@ -5,6 +5,11 @@ import addFileIcon from './image/addFileIcon.svg';
 import Error from './Error';
 import { IoHomeOutline, IoCameraOutline } from "react-icons/io5";
 import './DragNDrop.css';
+import { MdLogout } from "react-icons/md";
+import { FaRegUser } from "react-icons/fa";
+
+
+
 
 function DragNDrop() {
     const [files, setFiles] = useState([]);
@@ -30,15 +35,17 @@ function DragNDrop() {
 
     return (
         <>
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">Logo</Navbar.Brand>
+            <Navbar bg="light" expand="lg" className='bars'  >
+                <Navbar.Brand href="#home">Image Upload  </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#image-upload">Image Upload</Nav.Link>
+                        <Nav.Link href="#home"></Nav.Link>
+                        <Nav.Link href="#image-upload"></Nav.Link>
                     </Nav>
                     <Nav>
+
+                  
                         <NavDropdown title="Lindokuhle Mahlangu" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">lindo@gmail.com</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Reset Password</NavDropdown.Item>
@@ -55,15 +62,24 @@ function DragNDrop() {
                             <IoHomeOutline className="icon ma" /> Home
                         </button>
                         <button className="btn uplodBTN btn-block mb-5">
-                            <IoCameraOutline className="icon ma" /> Image Upload
+                            <IoCameraOutline className="icon ma  " /> Image Upload
                         </button>
                     </div>
-                    <button className="btn btn-danger btn-block mt-auto" onClick={handleLogout}>Log Out</button>
+                    <button className="btn logout btn-block mt-auto" onClick={handleLogout}>
+                        
+                    <MdLogout className="icon ma  " />
+                        
+                        Log Out</button>
                 </div>
-                <div className="container my-5 open">
-                    <h1 className="text-center mb-4 UploadHead">Image Upload</h1>
-                    <div className="card mx-auto" style={{ maxWidth: '600px' }}>
+                <div className="container my-2 open">
+                   
+                    <div className="card border-0" >
                         <div className="card-body">
+
+                        <h1 className="text-center mb-4 UploadHead">Image Upload</h1>
+
+
+
                             <div className="mb-4">
                                 <label htmlFor="title" className="form-label Imagetitle">Image Title</label>
                                 <input
@@ -84,25 +100,24 @@ function DragNDrop() {
                                     onChange={(e) => setDescription(e.target.value)}
                                 ></textarea>
                             </div>
-                            <div {...getRootProps({ className: "border border-2 border-primary p-4 text-center dropNDrag" })}>
+                            <div {...getRootProps({ className: "  dropNDrag" })}>
                                 <input {...getInputProps()} />
                                 <img className="mb-3" src={addFileIcon} alt="add file" style={{ height: '80px' }} />
                                 <p>Drag and Drop Files</p>
                                 <p>or </p>
                                 <button type="button" className="btn btn-primary">Upload</button>
                             </div>
-
-                            <div className="d-flex flex-wrap justify-content-center mt-3">
-                                {files.map((file) => (
-                                    <div className="m-2" key={file.name}>
-                                        <img
-                                            src={file.preview}
-                                            alt="thumbnail"
-                                            className="img-thumbnail"
-                                            style={{ width: '150px', height: '150px' }}
-                                            onLoad={() => {
-                                                URL.revokeObjectURL(file.preview);
-                                            }}
+                            <div className="flex justify-center flex-wrap "> 
+        {
+            files.map((file)=>(
+                <div className="image-preview m-2 p-4 w-40 h-40 " key={file.name}>
+                    <img src={file.preview} alt="thumnail"
+                    className='w-auto h-full rounded-md'
+                    onLoad={
+                        ()=>{
+                            URL.revokeObjectURL(file.preview)
+                        }
+                    }
                                         />
                                     </div>
                                 ))}
