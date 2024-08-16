d2a5a3a1155f72f977b415d8d779ec338268a592
Keasanda <Mlungisi716@gmail.com>
2024-08-16 16:02:48 +0200

    Amanda work

--- securewebsite.client/src/components/Home.jsx
+++ securewebsite.client/src/components/Home.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Home.css';
+import styles from './Home.module.css';
 import { IoFilterSharp, IoHomeOutline, IoCameraOutline } from "react-icons/io5";
 import { MdLogout } from "react-icons/md";
 import { GrGallery } from "react-icons/gr";
@@ -17,7 +17,7 @@ function Home() {
     const [loveCounts, setLoveCounts] = useState({}); // New state for love counts
     const [currentPage, setCurrentPage] = useState(1);
     const [searchQuery, setSearchQuery] = useState('');
-    const imagesPerPage = 6; // Adjusted for three per row
+    const imagesPerPage = 3; // Adjusted for three per row
 
     useEffect(() => {
         const storedUser = localStorage.getItem("user");
@@ -151,88 +151,89 @@ function Home() {
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     return (
-        <div className="content">
-            <div className="sidebarHome">
-                <header className='homelog'>Logo</header>
-                <nav className="navButoom">
+        <div className={styles.content}>
+    <div className={styles.sidebarHome}>
+        <img src="src/assets/Image Gallery.png"className={styles.logo} />
+        <nav className={styles.navButoom}>
                     <ul style={{ listStyle: 'none', padding: 0, margin: 0 }}>
                         <li>
-                            <button className="nav-buttonHome ho active">
-                                <IoHomeOutline className="icon hm" /> Home
+                        <button className={styles.homelog} onClick={() => window.location.href = '/home'}>
+                                <IoHomeOutline className="icon ma" /> Home
                             </button>
                         </li>
                         <li>
-                            <button className="nav-buttonHome ho" onClick={() => window.location.href = '/dragndrop'}>
-                                <IoCameraOutline className="icon hm" /> Image Upload
+                            <button className={styles.homelog} onClick={() => window.location.href = '/dragndrop'}>
+                                <IoCameraOutline className="icon ma" /> Image Upload
                             </button>
                         </li>
                         <li>
-                            <button className="nav-buttonHome myLb ho" onClick={() => window.location.href = '/MyGallery'}>
-                                <GrGallery className="icon hm" /> My Library
+                            <button className={styles.homelog} onClick={() => window.location.href = '/'}>
+                                <GrGallery className="icon ma" /> My Gallery
                             </button>
                         </li>
                     </ul>
                 </nav>
-                <button className='logout' onClick={handleLogout}>
-                    <MdLogout className="icon hm" /> Logout
-                </button>
+                <button className={styles.logout} onClick={handleLogout}>
+            <MdLogout className={styles.icon} /> Logout
+        </button>
             </div>
-            <div className="main-content">
-                <Navbar bg="light" expand="lg" className='homenav'>
-                    <Navbar.Brand style={{ marginLeft: '25px', fontFamily: 'Poppins', fontSize: "normal" }} href="#home"> Home <span>&#62;</span> </Navbar.Brand>
-                    <Nav className="me-auto"></Nav>
-                    <Nav>
-                        {userInfo ? (
-                            <NavDropdown title={<span><img src="src/assets/notebook-natural-laptop-macbook.jpg" alt="Profile" className="profile-image" /> {userInfo.userName}</span>}>
-                                <NavDropdown.Item>{userInfo.userEmail}</NavDropdown.Item>
-                                <NavDropdown.Item href="/LoggedInResetPassword">Reset Password</NavDropdown.Item>
-                            </NavDropdown>
-                        ) : (
-                            <Nav.Link href="/login">Login</Nav.Link>
-                        )}
-                    </Nav>
-                </Navbar>
-                <div className="search-bar">
-                    <input
-                        type="text"
-                        placeholder="Search for..."
-                        value={searchQuery}
-                        onChange={handleSearch}
-                    />
-                    <button className="filters-button"><IoFilterSharp className="icon" /> Filters</button>
-                </div>
-                <div className="image-gallery">
-                    {userInfo ? (
-                        currentImages.length > 0 ? (
-                            currentImages.map((image) => (
-                                <Link to={`/image/${image.imageId}`} key={image.imageId} className="card">
-                                    <div className="image-container">
-                                        <img src={image.imageURL} className="card-img-top" alt={image.title} />
-                                    </div>
-                                    <div className="card-title-overlay">
-                                        <h5>{image.title}</h5>
-                                        <p>{image.description}</p>
-                                    </div>
-                                    <div className="card-actions">
-                                        <div className="icon-group" onClick={() => toggleLove(image.imageId)}>
-                                            <FaHeart className="heart-icon" />
-                                            <span className="icon-count">{loveCounts[image.imageId] || 0}</span>
-                                        </div>
-                                        <div className="icon-group  commpush  ">
-                                            <FaComment className="comment-icon" />
-                                            <span className="icon-count">{commentCounts[image.imageId] || 0}</span>
-                                        </div>
-                                    </div>
-                                </Link>
-                            ))
-                        ) : (
-                            <p className="no-matches-message">No images match your search criteria.</p>
-                        )
-                    ) : (
-                        <p className="no-matches-message">Please log in to view images.</p>
-                    )}
-                </div>
-                <div className="pagination">
+            <div className={styles.mainContent}>
+        <Navbar bg="light" expand="lg" className={styles.homenav}>
+            <Navbar.Brand style={{ marginLeft: '25px', fontFamily: 'Poppins', fontSize: "normal" }} href="#home"> Home <span>&#62;</span> </Navbar.Brand>
+            <Nav className="me-auto"></Nav>
+            <Nav>
+            {userInfo ? (
+                    <NavDropdown title={<span><img src="src/assets/notebook-natural-laptop-macbook.jpg" alt="Profile" className={styles.profileImage} /> {userInfo.userName}</span>}>
+                        <NavDropdown.Item>{userInfo.userEmail}</NavDropdown.Item>
+                        <NavDropdown.Item href="/LoggedInResetPassword">Reset Password</NavDropdown.Item>
+                    </NavDropdown>
+                ) : (
+                    <Nav.Link href="/login">Login</Nav.Link>
+                )}
+            </Nav>
+        </Navbar>
+        <div className={styles.searchBar}>
+            <input
+                type="text"
+                placeholder="Search for..."
+                value={searchQuery}
+                onChange={handleSearch}
+                className={styles.searchInput}
+            />
+                    <button className={styles.filtersButton}><IoFilterSharp className={styles.icon} /> Filters</button>
+        </div>
+        <div className={styles.imageGallery}>
+            {userInfo ? (
+                currentImages.length > 0 ? (
+                    currentImages.map((image) => (
+                        <Link to={`/image/${image.imageId}`} key={image.imageId} className={styles.card}>
+                            <div className={styles.imageContainer}>
+                                <img src={image.imageURL} className="card-img-top" alt={image.title} />
+                            </div>
+                            <div className={styles.cardTitleOverlay}>
+                                <h5>{image.title}</h5>
+                                
+                            </div>
+                            <div className={styles.cardActions}>
+                                <div className={styles.iconGroup} onClick={() => toggleLove(image.imageId)}>
+                                    <FaHeart className={styles.heartIcon} />
+                                    <span className={styles.iconCount}>{loveCounts[image.imageId] || 0}</span>
+                                </div>
+                                <div className={`${styles.iconGroup} ${styles.commpush}`}>
+                                    <FaComment className={styles.commentIcon} />
+                                    <span className={styles.iconCount}>{commentCounts[image.imageId] || 0}</span>
+                                </div>
+                            </div>
+                        </Link>
+                    ))
+                ) : (
+                    <p className={styles.noMatchesMessage}>No images match your search criteria.</p>
+                )
+            ) : (
+                <p className={styles.noMatchesMessage}>Please log in to view images.</p>
+            )}
+        </div>
+        <div className="pagination">
                     {Array.from({ length: Math.ceil(filteredImages.length / imagesPerPage) }, (_, index) => (
                         <button
                             key={index + 1}
